9812b7ace393f9f3c097a9225265101f73344382
lee <shida23577@163.com>
2015-11-24 19:14:52 +0800

    func wrap

--- examples/onvif_host.py
+++ examples/onvif_host.py
@@ -2,6 +2,7 @@
 from onvif import ONVIFCamera
 import onvif
 import os
+import sys
 try:
     import xml.etree.cElementTree as ET
 except:
@@ -17,14 +18,18 @@ class onvif_host:
         self.port = port
         self.username = username
         self.password = password
-        wsdl_path = os.path.join(os.path.dirname(onvif.__file__), os.path.pardir, "wsdl")
-        self.camera = ONVIFCamera(ip, port, username, password, wsdl_path)
-    def get_stream_url(self, stream_urls=None):
+        if sys.platform == 'win32':
+            wsdl_path = os.path.join(os.path.dirname(onvif.__file__), os.path.pardir, "wsdl")
+            self.camera = ONVIFCamera(ip, port, username, password, wsdl_path)
+        else:
+            self.camera = ONVIFCamera(ip, port, username, password)
+    #def get_stream_url(self, stream_urls=None):
+    def get_stream_url(self, out_data=None):
         media_capability_name ='Meda'
         media_info = self.camera.devicemgmt.GetCapabilities({'Categroy':media_capability_name})
-        if stream_urls is None:
+        if out_data is None:
             print("come here")
-            stream_urls = list()
+            out_data = list()
 
 
         #print(media_info)
@@ -39,14 +44,30 @@ class onvif_host:
                 #print(stream_url.Uri)
                 url_node  = ET.SubElement(url_nodes,'stream_url')
                 url_node.text = stream_url.Uri
-            url_tree = ET.ElementTree(url_nodes)
+            #url_tree = ET.ElementTree(url_nodes)
             #ET.dump(url_nodes)
-            if type(stream_urls) == list:
-                stream_urls.append(ET.tostring(url_nodes))
-            elif type(stream_urls) == str:
-                stream_urls.join(ET.tostring(url_nodes))
-            print(stream_urls)
+            if type(out_data) == list:
+                out_data.append(ET.tostring(url_nodes))
+            elif type(out_data) == str:
+                out_data.join(ET.tostring(url_nodes))
+            print(out_data)
 
+    def get_device_status(self, out_data, out_data_type):
+        """get device current status"""
+        return 0
+    def add_user(self, out_data, username, password, level=None):
+        """add a user to device"""
+        if level is None:
+            level = 'Operator'
+        print(locals())
+        user = {'User':{'Username':username, 'Password':password, 'UserLevel':level}}
+        out_response = self.camera.devicemgmt.CreateUsers(user)
+        print(out_response)
+    def del_user(self, username):
+        """ delete a device user."""
+        user = {'Username':username}
+        out_response = self.camera.devicemgmt.DeleteUsers(user)
+        print(out_response)
 
 def register_device(device_id, ip, port, username, password):
     print locals()
@@ -60,20 +81,38 @@ def unregister_device(device_id):
 def request_onvif_cmd(device_id, cmd, out_params = None, **in_params):
     print locals()
     if device_list.has_key(device_id):
-        if cmd in dir(onvif_host):
+        if cmd in dir(onvif_host) and hasattr(device_list[device_id], cmd):
             cmd_func = getattr(device_list[device_id], cmd)
             print(len(in_params))
-            print(out_params)
+            #print(out_params)
             if 0 < len(in_params):
-                in_param = in_params.items()
-
+                #in_param = in_params.items()
+                in_param = in_params.values()
+                print(in_param)
                 if out_params is not None:
-                    cmd_func(in_params, out_params)
+                    if 1 == len(in_param):
+                        cmd_func(out_params, in_param[0])
+                    elif 2 == len(in_param):
+                        cmd_func(out_params, in_param[0], in_param[1])
+                    elif 3 == len(in_param):
+                        cmd_func(out_params, in_param[0], in_param[1], in_param[2])
+                    elif 4 == len(in_param):
+                        cmd_func(out_params, in_param[0], in_param[1], in_param[2], in_param[3])
+                    elif 5 == len(in_param):
+                        cmd_func(out_params, in_param[0], in_param[1], in_param[2], in_param[3], in_param[4])
                 else:
-                    cmd_func(in_param)
+                    if 1 == len(in_param):
+                        cmd_func(in_params[0])
+                    elif 2 == len(in_param):
+                        cmd_func(in_param[0], in_param[1])
+                    elif 3 == len(in_param):
+                        cmd_func(in_param[0], in_param[1], in_param[2])
+                    elif 4 == len(in_param):
+                        cmd_func(in_param[0], in_param[1], in_param[2], in_param[3])
+                    elif 5 == len(in_param):
+                        cmd_func(in_param[0], in_param[1], in_param[2], in_param[3], in_param[4])
             else:
                 if out_params is not None:
-
                     cmd_func(out_params)
                 elif out_params is None:
                     cmd_func()
@@ -87,11 +126,18 @@ if __name__ == '__main__':
     username = 'admin'
     password = '12345'
     register_device(device_id, ip, port, username, password)
-    urls = list()
-    request_onvif_cmd(device_id,'get_stream_url', out_params = urls)
-    urls_ext = str()
-    request_onvif_cmd(device_id,'get_stream_url', out_params = urls_ext)
-    print(urls)
-    print(urls_ext)
+    #urls = list()
+    #request_onvif_cmd(device_id,'get_stream_url', out_params = urls)
+    #print(urls)
+
+    '''
+    test_host = onvif_host(ip, port, username, password)
+    out_user_response = list()
+    params = {'out_data':out_user_response, 'username':'onvif_test', 'password':'12345'}
+    #test_host.add_user(out_user_response, username='onvif_test', password='12345')
+    test_host.add_user(params)
+    '''
+    out_user_response = list()
+    request_onvif_cmd(device_id,'add_user', out_params = out_user_response, username='onvif_test124', password='12345')
     raw_input()
 
