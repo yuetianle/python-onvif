efdb1bd78044e9ca26fdc037a7fa0dfffafd5a2a
lee <shida23577@163.com>
2015-11-25 15:05:40 +0800

    add logger and function tmplate add user control

--- examples/onvif_host.py
+++ examples/onvif_host.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python
+#coding=utf8
 from onvif import ONVIFCamera
 import onvif
 import os
+import types
 import sys
+import inspect
+
+import logging
+
+# 使用一个名字为fib的logger
+logger = logging.getLogger('onvif_host')
+
+# 设置logger的level为DEBUG
+logger.setLevel(logging.DEBUG)
+#logger.setLevel(logging.WARN)
+
+# 创建一个输出日志到控制台的StreamHandler
+hdr = logging.StreamHandler()
+formatter = logging.Formatter('[%(asctime)s] [%(levelname)s] [%(name)s] [%(filename)s:%(funcName)s:%(lineno)s] : %(message)s')
+hdr.setFormatter(formatter)
+# 给logger添加上handler
+logger.addHandler(hdr)
+
 try:
     import xml.etree.cElementTree as ET
 except:
@@ -23,72 +43,125 @@ class onvif_host:
             self.camera = ONVIFCamera(ip, port, username, password, wsdl_path)
         else:
             self.camera = ONVIFCamera(ip, port, username, password)
-    #def get_stream_url(self, stream_urls=None):
+
+    def __check_user_exist__(self, username, users = None):
+        user_lists = self.camera.devicemgmt.GetUsers()
+        if users is None:
+            users = dict()
+        users.clear()
+        user_name_lists = list()
+        for user_item in user_lists:
+            user_name_lists.append(user_item.Username)
+            if not users.has_key(user_item.Username) and user_item.Username == username:
+                if hasattr(user_item, 'Username'):
+                    users['Username'] = user_item.Username
+                if hasattr(user_item, 'Password'):
+                    users['Password'] = user_item.Password
+                if hasattr(user_item, 'UserLevel'):
+                    users['UserLevel'] = user_item.UserLevel
+        if username not in user_name_lists:
+            return False
+        else:
+            return True
+
     def get_stream_url(self, out_data=None):
         media_capability_name ='Meda'
         media_info = self.camera.devicemgmt.GetCapabilities({'Categroy':media_capability_name})
         if out_data is None:
-            print("come here")
             out_data = list()
 
-
-        #print(media_info)
         if media_info.Media.StreamingCapabilities.RTP_RTSP_TCP or media_info.Media.StreamingCapabilities.RTP_TCP:
             media_service = self.camera.create_media_service()
             profiles = media_service.GetProfiles()
             url_nodes = ET.Element('stream_url_lists')
             for item in profiles:
-                #print(item._token)
                 stream = {'StreamSetup':{'StreamType':'RTP_unicast','Transport':'RTSP'}, 'ProfileToken':item._token}
                 stream_url = media_service.GetStreamUri(stream)
-                #print(stream_url.Uri)
                 url_node  = ET.SubElement(url_nodes,'stream_url')
                 url_node.text = stream_url.Uri
-            #url_tree = ET.ElementTree(url_nodes)
-            #ET.dump(url_nodes)
             if type(out_data) == list:
                 out_data.append(ET.tostring(url_nodes))
             elif type(out_data) == str:
                 out_data.join(ET.tostring(url_nodes))
-            print(out_data)
-
     def get_device_status(self, out_data, out_data_type):
         """get device current status"""
         return 0
     def add_user(self, out_data, username, password, level=None):
         """add a user to device"""
+        logger.debug(locals())
         if level is None:
             level = 'Operator'
-        print(locals())
         user = {'User':{'Username':username, 'Password':password, 'UserLevel':level}}
-        out_response = self.camera.devicemgmt.CreateUsers(user)
-        print(out_response)
-    def del_user(self, username):
+        user_lists_node = ET.Element('user_lists')
+        user_content    = ET.SubElement(user_lists_node, 'user')
+        user_content.set('name', username)
+        if self.__check_user_exist__(username):
+            user_content.text = 'False'
+            user_content.set('error_message', 'user exist')
+        else:
+            try:
+                out_response = self.camera.devicemgmt.CreateUsers(user)
+                user_content.text = 'True'
+            except:
+                user_content.text = 'False'
+        out_data.append(ET.tostring(user_lists_node))
+    def del_user(self, out_data, username):
         """ delete a device user."""
+        logger.debug(locals())
         user = {'Username':username}
-        out_response = self.camera.devicemgmt.DeleteUsers(user)
-        print(out_response)
+        user_lists_node = ET.Element('user_lists')
+        user_content    = ET.SubElement(user_lists_node, 'user')
+        user_content.set('name', username)
+        if self.__check_user_exist__(username):
+            out_response = self.camera.devicemgmt.DeleteUsers(user)
+            user_content.text = 'True'
+        else:
+            user_content.text = 'False'
+            user_content.set('error_message', 'user not exist')
+        if type(out_data) == types.ListType:
+            out_data.append(ET.tostring(user_lists_node))
+
+    def alter_user(self, out_data, username, level, password=None):
+        """alter device user info"""
+        logger.debug(locals())
+        users = dict()
+        user_lists_node = ET.Element('user_lists')
+        user_content    = ET.SubElement(user_lists_node, 'user')
+        user_content.set('name', username)
+        if self.__check_user_exist__(username, users):
+            if password is None:
+                user = {'User':{'Username':username, 'UserLevel':level}}
+                self.camera.devicemgmt.SetUser(user)
+            else:
+                user = {'User':{'Username':username, 'UserLevel':level, 'Password':password}}
+                self.camera.devicemgmt.SetUser(user)
+            user_content.text = 'True'
+        else:
+            logger.warning("user %s not exitst", username)
+            user_content.text = 'False'
+        out_data.append(ET.tostring(user_lists_node))
+
 
 def register_device(device_id, ip, port, username, password):
-    print locals()
+    logger.debug(locals())
     if not device_list.has_key(device_id):
         client = onvif_host(ip, port, username, password)
         device_list[device_id] = client
-    print(len(device_list))
 def unregister_device(device_id):
     if device_list.has_key(device_id):
         del device_list[device_id]
 def request_onvif_cmd(device_id, cmd, out_params = None, **in_params):
-    print locals()
+    logger.debug(locals())
     if device_list.has_key(device_id):
         if cmd in dir(onvif_host) and hasattr(device_list[device_id], cmd):
             cmd_func = getattr(device_list[device_id], cmd)
-            print(len(in_params))
-            #print(out_params)
+            params_lists = []
+            for call_args in inspect.getargspec(cmd_func).args:
+                if in_params.has_key(call_args):
+                    params_lists.append(in_params.get(call_args))
+            logger.debug("cmd=%s args:%s args_value:%s", cmd, inspect.getargspec(cmd_func).args, params_lists)
             if 0 < len(in_params):
-                #in_param = in_params.items()
-                in_param = in_params.values()
-                print(in_param)
+                in_param = params_lists
                 if out_params is not None:
                     if 1 == len(in_param):
                         cmd_func(out_params, in_param[0])
@@ -117,8 +190,6 @@ def request_onvif_cmd(device_id, cmd, out_params = None, **in_params):
                 elif out_params is None:
                     cmd_func()
 
-
-
 if __name__ == '__main__':
     device_id = '172.16.1.221'
     ip = '172.16.1.221'
@@ -126,9 +197,8 @@ if __name__ == '__main__':
     username = 'admin'
     password = '12345'
     register_device(device_id, ip, port, username, password)
-    #urls = list()
-    #request_onvif_cmd(device_id,'get_stream_url', out_params = urls)
-    #print(urls)
+    urls = list()
+    request_onvif_cmd(device_id,'get_stream_url', out_params = urls)
 
     '''
     test_host = onvif_host(ip, port, username, password)
@@ -139,5 +209,10 @@ if __name__ == '__main__':
     '''
     out_user_response = list()
     request_onvif_cmd(device_id,'add_user', out_params = out_user_response, username='onvif_test124', password='12345')
+    out_user_response = list()
+    #request_onvif_cmd(device_id,'del_user', out_params = out_user_response, username='onvif_test124')
+    out_user_response = list()
+    request_onvif_cmd(device_id,'alter_user', out_params = out_user_response, username='onvif_test124', level = 'User', password = 'OnvifTest123')
+    #request_onvif_cmd(device_id,'alter_user', out_params = out_user_response, username='onvif_test124', level = 'User')
     raw_input()
 
